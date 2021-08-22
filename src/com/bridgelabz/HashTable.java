5ed0f33f0134ee37d162fcbf97fc08b299053724
SUDIPP22 <panja370@gmail.com>
2021-08-22 18:18:51 +0530

    Added : Welcome Message

---
+++ src/com/bridgelabz/HashTable.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+/**
+ * Purpose - To create Hash Map using Hash Tables
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-0
+ */
+public class HashTable {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Hash Tables");
+    }
+}
